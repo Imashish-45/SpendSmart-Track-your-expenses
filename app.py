ae59ace813527fd1113b96a73e0bdd632fc0a482
aupadhyay <aupadhyay@controlcase.com>
2024-05-19 18:22:20 +0530

    ini

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect
+
+app =  Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/add_expense', methods=['GET', 'POST'])
+def add_expense():
+    if request.method == 'POST':
+        # Handle form submission and add expense
+
+        
+        
+        return redirect(url_for('index'))
+    return render_template('add_expense.html')
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
